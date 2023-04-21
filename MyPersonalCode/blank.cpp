7d496f502c130dd8bcb9675f47127ef77cabb127
CCER N. KSHETRI <95275748+snsk7@users.noreply.github.com>
2023-04-22 04:12:56 +0545

    s

--- MyPersonalCode/blank.cpp
+++ MyPersonalCode/blank.cpp
@@ -1,38 +1,19 @@
-#include <bits/stdc++.h>
-using namespace std;
-string longestevenword(string str) {
-   int len = str.length();
-   int i = 0; int currWordlen = 0;
-   int maxWordLen = 0;
-   int stringPointer = -1;
-   while (i < len) {
-      if (str[i] == ' ') {
-         if (currWordlen % 2 == 0) {
-            if (maxWordLen < currWordlen) {
-               maxWordLen = currWordlen;
-               stringPointer = i - currWordlen;
-            }
-         }
-         currWordlen = 0;
-      }
-      else {
-         currWordlen++;
-      }
-      i++;
-   }
-   if (currWordlen % 2 == 0) {
-      if (maxWordLen < currWordlen) {
-         maxWordLen = currWordlen; 
-         stringPointer = i - currWordlen;
-      }
-   }
-   if (stringPointer == -1)
-      return "Not Found!";
-   return str.substr(stringPointer, maxWordLen);
-}
-int main() {
-   string str;
-   getline(cin,str);
-   cout<<longestevenword(str)<<"'";
-   return 0;
+#include<stdio.h>
+
+int main(){
+    int arr[5];
+    int (*ptr)[5];
+    for(int i=0;i<5;i++)
+        scanf("%d",&arr[i]);
+    ptr=&arr;
+    int spnum=0;
+    for(int i=0;i<4;i++){
+        spnum=*ptr[i]+*ptr[i+1];
+    }
+    printf("the sum is %d\n",spnum);
+    
+    
+    
+    
+    return 0;
 }
