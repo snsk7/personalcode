be4d586a19a620c1733c22e5b12a5a85ae14376a
CCER N. KSHETRI <95275748+snsk7@users.noreply.github.com>
2023-06-30 11:57:18 +0545

    Add files via upload

---
+++ java/lab_1/Fourth.java
@@ -0,0 +1,10 @@
+public class Fourth{
+	public static final double PI = 3.14159;
+	public static void main(String[] args){
+		double radius = 5.0;
+		double area = PI * radius * radius;
+		System.out.println("The area of circle is: "+area);
+
+
+	}
+}
